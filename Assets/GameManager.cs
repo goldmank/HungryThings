796238735416ab8d8fb9cd8dccd7ba89756cb7df
goldmank <goldmank@gmail.com>
2021-06-16 16:44:14 +0300

    add GameAnalytics

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameAnalyticsSDK;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(InitializeGameAnalytics());
+    }
+
+    private IEnumerator InitializeGameAnalytics()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GameAnalytics.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

08bbb4f0c221de1a1bc79fc09f5a13950915a985
goldmank <goldmank@gmail.com>
2021-06-16 21:04:50 +0300

    working template

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Facebook.Unity;
+using Firebase.Analytics;
 using GameAnalyticsSDK;
 using UnityEngine;
 
@@ -9,23 +10,49 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        InitFirebase();
+            
         FB.Init(() =>
         {
             Debug.Log("FB init completed");
         });
+
+        StartCoroutine(InitGameAnalytics());
         
-        StartCoroutine(InitializeGameAnalytics());
+        IronSource.Agent.init ("fd8b0c69");
+        IronSource.Agent.validateIntegration();
+
+    }
+    
+    private void OnApplicationPause(bool isPaused) {                 
+        IronSource.Agent.onApplicationPause(isPaused);
     }
 
-    private IEnumerator InitializeGameAnalytics()
+    private void InitFirebase()
     {
-        yield return new WaitForSeconds(0.5f);
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+            FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventLevelStart, new Parameter(FirebaseAnalytics.ParameterLevelName, "Start"));
+            
+            Debug.Log("dependencyStatus:" + task.Result);
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+    
+                var app = Firebase.FirebaseApp.DefaultInstance;
+            } else {
+                UnityEngine.Debug.LogError(System.String.Format(
+                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+    private IEnumerator InitGameAnalytics()
+    {
+        yield return new WaitForSeconds(0.3f);
         GameAnalytics.Initialize();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
